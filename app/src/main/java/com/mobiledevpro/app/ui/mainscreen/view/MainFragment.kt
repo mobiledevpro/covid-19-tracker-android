b4f4fe1d2c8e8830332e4c5b087c5a953c17f404
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2020-03-14 03:06:28 +0200

    Prepared a main screen

--- app/src/main/java/com/mobiledevpro/app/ui/mainscreen/view/MainFragment.kt
+++ app/src/main/java/com/mobiledevpro/app/ui/mainscreen/view/MainFragment.kt
@@ -1,7 +1,6 @@
 package com.mobiledevpro.app.ui.mainscreen.view
 
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
 import androidx.lifecycle.Observer
 import com.mobiledevpro.app.R
@@ -31,17 +30,7 @@ class MainFragment : BaseFragment() {
 
     override fun getAppBarTitle() = R.string.app_name_main
 
-    override fun getOptionsMenuResId() = R.menu.menu_main_fragment
-
     override fun getHomeAsUpIndicatorIcon() = R.drawable.ic_close_24dp
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean =
-            when (item.itemId) {
-                R.id.menu_action_crash -> {
-                    throw RuntimeException("Test crash")
-                }
-                else -> super.onOptionsItemSelected(item)
-            }
 
     override fun populateView(view: View, savedInstanceState: Bundle?): View {
         //databinding
