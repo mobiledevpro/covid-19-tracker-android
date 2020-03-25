352829fa2c7527617f302b05d995c8cc4cf66f20
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2020-03-25 13:57:59 +0200

    Fixes

--- data-local/src/main/java/com/mobiledevpro/local/database/statistic/model/CachedStatisticCountryWithDailyStatistic.kt
+++ data-local/src/main/java/com/mobiledevpro/local/database/statistic/model/CachedStatisticCountryWithDailyStatistic.kt
@@ -1,15 +1,16 @@
 package com.mobiledevpro.local.database.statistic.model
 
 import androidx.room.Embedded
+import androidx.room.Relation
 
 class CachedStatisticCountryWithDailyStatistic {
 
     @Embedded
     var country: CachedStatisticCountry? = null
 
-//    @Relation(
-//        parentColumn = "country",
-//        entityColumn = "country"
-//    )
+    @Relation(
+        parentColumn = "province",
+        entityColumn = "province"
+    )
     var dayStatistic: List<CachedDayTotalCountryStatistic> = listOf()
 }
